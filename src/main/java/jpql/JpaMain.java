e4622c237fbbdb9d30c5545181709bf249bf20d5
daewancha <dwcha0524@gmail.com>
2022-03-19 17:03:41 +0900

    Initial commit

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,50 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            //반환타입
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            //결과조회 API
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+//            List<Member> resultList = query1.getResultList();
+//
+//            TypedQuery<Member> query2 = em.createQuery("select m from Member m", Member.class);
+//            Member singleResult = query2.getSingleResult();
+
+            //파라미터 바인딩
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username", "member1");
+            Member singleResult = query.getSingleResult();
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
