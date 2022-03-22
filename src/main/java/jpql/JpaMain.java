c6b927652699107f944e64cb93d38583e2cb69f1
daewancha <dwcha0524@gmail.com>
2022-03-22 23:14:35 +0900

    jpql - fetch 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,24 +15,49 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            //JPQL 타입 표현
-            String query = "select m.username, 'HELLO', TRUE From Member m";
-            List<Object[]> result = em.createQuery(query)
+            String query = "select distinct t From Team t join fetch t.members";
+
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + ", " + team.getMembers().size());
             }
+
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " +member.getTeam().getName());
+//                //회원1, 팀A(SQL)
+//                //회원2, 팀A(1차캐시)
+//                //회원3, 팀B(SQL)
+//
+//                //회원 100명 -> N + 1
+//            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -10,12 +10,28 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     @Enumerated(EnumType.STRING)
     private MemberType type;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
+    }
 
     public Long getId() {
         return id;
